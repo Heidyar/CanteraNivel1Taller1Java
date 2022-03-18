00f5c770221b3f45a6348e87ac5d6c2bdec7679f
Heidy Dugarte <heidyar34@hotmail.com>
2022-03-18 11:00:29 -0500

    CanteraNivel1htmlCss

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+var getData = function (){
+    var name = document.getElementById("name").value;
+    var correo = document.getElementById("correo").value;
+    var asunto = document.getElementById("asunto").value;
+    var mensaje = document.getElementById("mensaje").value;
+    console.log(name + " " + correo + " " + asunto + " " + mensaje);
+}
